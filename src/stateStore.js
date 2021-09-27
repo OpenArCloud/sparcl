782ff7396d5c00651813ff70d299106a78bd54db
soeroesg <gabor.soros@gmail.com>
2021-09-27 22:57:57 +0200

    fixed crash on iOS at startup (navigator.permissions is undefined)

--- src/stateStore.js
+++ src/stateStore.js
@@ -35,6 +35,11 @@ export const isLocationAccessAllowed = readable(false, (set) => {
     let currentResult;
     const stateResult = (state) => state === 'granted';
 
+    // NOTE: navigator.permissions is undefined on iOS
+    if (navigator == undefined || navigator.permissions == undefined) {
+        return () => set(false);
+    }
+
     navigator.permissions.query({name:'geolocation'})
         .then((result) => {
             currentResult = result;
