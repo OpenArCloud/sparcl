8e2a544a14790f0afc7f78e155a64e4a710c82a2
Tamas Rigo <tamas.rigo@nokia.com>
2024-03-13 15:55:29 +0100

    fix bug of rendering p2p contents multiple times

--- src/experiments/oarc/ismar2021multi/Viewer.svelte
+++ src/experiments/oarc/ismar2021multi/Viewer.svelte
@@ -31,7 +31,7 @@
     setContext('state', parentState);
 
     $: {
-        if ($globalIsLocalized && $recentLocalisation?.geopose?.position) {
+        if ($recentLocalisation?.geopose?.position) {
             const assets = getAutomergeDocumentData();
             if (assets) {
                 for (const asset of assets) {
